6c3636696e666b3dcffb4dc46254483ac9844cea
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-20 11:47:25 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize(title)
+    @title = title
+  end
+
+  class Artist
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+    
+  end
+end
